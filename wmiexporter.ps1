6bbe01b1b9bdd1ddfd18be566aaa1ddc7f7ef3ec
Anders Østhus <anders.osthus@geodata.no>
2020-03-07 20:48:03 +0100

    Added WMI Exporter install script

---
+++ wmiexporter.ps1
@@ -0,0 +1,13 @@
+param(
+  [parameter(Mandatory=$true)] $Version
+)
+
+$DownloadDirectory = (Join-Path -Path (Get-Item Env:TEMP).Value -ChildPath "wmi")
+New-Item -Path $DownloadDirectory -ItemType "directory"
+wget "https://github.com/martinlindhe/wmi_exporter/releases/download/v$Version/wmi_exporter-$Version-amd64.msi" -OutFile "$DownloadDirectory/wmi.msi"
+
+$logFile = "c:/wmi_install.log"
+$file = Get-Item -Path "$DownloadDirectory/wmi.msi"
+$MSIArguments = @("/i", ('"{0}"' -f $file.fullname), "/qn", "/L*v", $logFile)
+
+Remove-Item -Path $DownloadDirectory -Recurse
