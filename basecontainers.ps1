4f5f0b217ec111984c92e57a89b3e0f55424ace0
Anders Østhus <anders.osthus@.geodata.no>
2021-01-19 10:25:31 +0100

    add net48 runtime image as base pull

--- basecontainers.ps1
+++ basecontainers.ps1
@@ -4,6 +4,7 @@ $WindowsVersion = $ComputerInfo.WindowsVersion
 
 docker pull "mcr.microsoft.com/windows/nanoserver:$WindowsVersion"
 docker pull "mcr.microsoft.com/windows/servercore:$WindowsVersion"
+docker pull "mcr.microsoft.com/dotnet/framework/runtime:4.8-windowsservercore-ltsc2019"
 
 docker tag "mcr.microsoft.com/windows/nanoserver:$WindowsVersion" windows/nanoserver:latest
 docker tag "mcr.microsoft.com/windows/nanoserver:$WindowsVersion" microsoft/nanoserver:latest
